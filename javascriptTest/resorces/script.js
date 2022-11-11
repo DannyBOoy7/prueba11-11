60b1ac12351e7692eefe31bcca1d1020cd2c20d8
Daniel Cárdenas <danyboyzaza.pro@gmail.com>
2022-11-11 10:21:18 -0500

    lol

---
+++ javascriptTest/resorces/script.js
@@ -0,0 +1,11 @@
+console.log("BIENVENIDO A LA EVALUACIÓN DE JAVASCRIPT!!!")
+function mainFunction(){
+    
+    printText=document.getElementById("tex").innerHTML
+    alert(printText)
+    updateHeader=document.getElementById("cab").innerHTML="CABECERA ACTUALIZADA."
+
+    
+      
+}
+
